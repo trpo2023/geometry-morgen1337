d15e5fcaed640efdc8cd57fae69c50529a10e61c
unknown <asasin8849@gmail.com>
2023-02-26 20:29:05 +0700

    One WKT circle read from txt and print on screen

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-int main()
 
-{
-    printf("Hello World");
+struct Point{
+    int x;
+    int y;
+};
+
+struct WKTcircle {
+    struct Point point;
+    int number;
+};
+
+int main(){
+    FILE *myfile;
+    myfile = fopen("sas.txt", "r");
+    int a,b;
+    struct WKTcircle c1;
+    fscanf(myfile, "circle(%d %d, %d)", &c1.point.x, &c1.point.y, &c1.number);
+    printf("%d %d  %d", c1.point.x, c1.point.y, c1.number);
     return 0;
 }
