977ee66d34b95d7f05f2ec5c1dd19c6ed5985e94
unknown <asasin8849@gmail.com>
2023-02-27 02:12:10 +0700

    programm upgrade

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct Point{
+struct Point {
     float x;
     float y;
 };
@@ -11,16 +12,33 @@ struct WKTcircle {
     float number;
 };
 
-int main(){
-    FILE *myfile;
+int main()
+{
+    FILE* myfile;
     myfile = fopen("sas.txt", "r");
-
+    char* type;
     char line[100];
+    char line1[100];
 
     struct WKTcircle c1;
-    if ((fgets(line, 100, myfile))){
-    sscanf(line, "circle(%f %f, %f)", &c1.point.x, &c1.point.y, &c1.number);
-    printf("circle(%f %f  %f) \n", c1.point.x, c1.point.y, c1.number);
+    while ((fgets(line, 100, myfile)) != 0) {
+        strcpy(line1, line);
+        type = strtok(line1, "(");
+        if (strcmp(type, "circle") == 0) {
+            if ((sscanf(line,
+                        "circle(%f %f, %f)",
+                        &c1.point.x,
+                        &c1.point.y,
+                        &c1.number))
+                == 3)
+                printf("circle(%f %f  %f) \n",
+                       c1.point.x,
+                       c1.point.y,
+                       c1.number);
+            else
+                printf("inccorect data format \n");
+        } else
+            printf("inccorect data format \n");
     }
     return 0;
 }
