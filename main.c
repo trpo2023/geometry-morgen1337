2b0c29bf1a5d5051018dabb0fd22ee83a2c1b0fd
unknown <asasin8849@gmail.com>
2023-02-26 23:43:23 +0700

    circle WKT print

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Point{
-    int x;
-    int y;
+    float x;
+    float y;
 };
 
 struct WKTcircle {
     struct Point point;
-    int number;
+    float number;
 };
 
 int main(){
     FILE *myfile;
     myfile = fopen("sas.txt", "r");
-    int a,b;
+
+    char line[100];
+
     struct WKTcircle c1;
-    fscanf(myfile, "circle(%d %d, %d)", &c1.point.x, &c1.point.y, &c1.number);
-    printf("%d %d  %d", c1.point.x, c1.point.y, c1.number);
+    if ((fgets(line, 100, myfile))){
+    sscanf(line, "circle(%f %f, %f)", &c1.point.x, &c1.point.y, &c1.number);
+    printf("circle(%f %f  %f) \n", c1.point.x, c1.point.y, c1.number);
+    }
     return 0;
 }
