aee2cbc43302ec4a74c91157caf414f4209ed314
Pavel Buharin <asasin8849@gmail.com>
2023-03-01 08:18:15 +0700

    minor fixes

--- main.c
+++ main.c
@@ -3,42 +3,34 @@
 #include <string.h>
 
 struct Point {
-    float x;
-    float y;
+  float x;
+  float y;
 };
 
 struct WKTcircle {
-    struct Point point;
-    float number;
+  struct Point point;
+  float number;
 };
 
-int main()
-{
-    FILE* myfile;
-    myfile = fopen("sas.txt", "r");
-    char* type;
-    char line[100];
-    char line1[100];
+int main() {
+  FILE *myfile;
+  myfile = fopen("sas.txt", "r");
+  char *type;
+  char line[100];
+  char line1[100];
 
-    struct WKTcircle c1;
-    while ((fgets(line, 100, myfile)) != 0) {
-        strcpy(line1, line);
-        type = strtok(line1, "(");
-        if (strcmp(type, "circle") == 0) {
-            if ((sscanf(line,
-                        "circle(%f %f, %f)",
-                        &c1.point.x,
-                        &c1.point.y,
-                        &c1.number))
-                == 3)
-                printf("circle(%f %f  %f) \n",
-                       c1.point.x,
-                       c1.point.y,
-                       c1.number);
-            else
-                printf("incorect data of points of circle \n");
-        } else
-            printf("incorect type format \n");
-    }
-    return 0;
+  struct WKTcircle c1;
+  while ((fgets(line, 100, myfile)) != 0) {
+    strcpy(line1, line);
+    type = strtok(line1, "(");
+    if (strcmp(type, "circle") == 0) {
+      if ((sscanf(line, "circle(%f %f, %f)", &c1.point.x, &c1.point.y,
+                  &c1.number)) == 3)
+        printf("circle(%f %f  %f) \n", c1.point.x, c1.point.y, c1.number);
+      else
+        printf("incorrect data of points of circle. Expected NUM NUM, NUM \n");
+    } else
+            printf("Incorrect type of figure format. Expected: \"Circle\ "n");
+  }
+  return 0;
 }
